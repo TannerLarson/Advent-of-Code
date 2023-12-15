9dad8a63ad80f157f0ea163d80f784972575c876
Tanner Larson <tannerl@qualtrics.com>
2023-12-15 14:38:51 -0700

    finished day8

--- 2023/rust/day8-23/src/main.rs
+++ 2023/rust/day8-23/src/main.rs
@@ -1,4 +1,5 @@
 use core::fmt;
+use num_integer::Integer;
 use std::{collections::HashMap, str::FromStr};
 
 fn main() {
@@ -12,21 +13,50 @@ fn main() {
         })
         .collect();
 
-    for (id, node) in node_map.clone() {
-        println!("{}: {:?}", id, node);
-    }
+    // let mut current_node_id = "AAA".to_string();
+    // let mut steps = 0;
+    // while current_node_id != "ZZZ" {
+    //     current_node_id = node_map
+    //         .get(&current_node_id)
+    //         .unwrap()
+    //         .next(instructions.next().unwrap());
+    //     steps += 1;
+    // }
+    // println!("Part 1: {}", steps);
 
-    let mut current_node_id = "AAA".to_string();
+    let mut node_ids: Vec<String> = node_map
+        .iter()
+        .filter(|&(k, _)| k.ends_with('A'))
+        .map(|(_, v)| v.clone().id)
+        .collect();
+    println!("{:?}", node_ids);
+
     let mut steps = 0;
-    while current_node_id != "ZZZ" {
-        println!("{}", current_node_id);
-        current_node_id = node_map
-            .get(&current_node_id)
-            .unwrap()
-            .next(instructions.next().unwrap());
+    let mut node_loops: Vec<u64> = Vec::new();
+    println!("Node IDs: {:?}", node_ids);
+    println!("Node loops: {:?}", node_loops);
+    while !node_ids.is_empty() {
         steps += 1;
+        let instruction = instructions.next().unwrap();
+        node_ids = node_ids
+            .iter()
+            .map(|id| node_map.get(id).unwrap().next(instruction))
+            .collect();
+        for id in node_ids.clone() {
+            if id.ends_with('Z') {
+                node_loops.push(steps)
+            }
+        }
+        node_ids.retain(|id| !id.ends_with('Z'));
+        println!("Node IDs: {:?}", node_ids);
+        println!("Node loops: {:?}", node_loops);
     }
-    println!("{}", steps)
+    println!(
+        "Part 2: {}",
+        node_loops
+            .iter()
+            .fold(1, |lcm_so_far, &next_number| lcm_so_far.lcm(&next_number))
+    );
 }
 
 #[derive(Clone)]
