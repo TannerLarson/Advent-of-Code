f729000c18e4d1c4417b918dc3e9987062a34eb9
Tanner Larson <tannerl@qualtrics.com>
2023-12-14 22:22:59 -0700

    part 1 done

---
+++ 2023/rust/day8-23/src/main.rs
@@ -0,0 +1,70 @@
+use core::fmt;
+use std::{collections::HashMap, str::FromStr};
+
+fn main() {
+    let mut lines = include_str!("input.txt").lines();
+    let mut instructions = lines.next().unwrap().chars().cycle();
+    let node_map: HashMap<String, Node> = lines
+        .skip(1)
+        .map(|line| {
+            let node: Node = line.parse().unwrap();
+            (node.clone().id, node)
+        })
+        .collect();
+
+    for (id, node) in node_map.clone() {
+        println!("{}: {:?}", id, node);
+    }
+
+    let mut current_node_id = "AAA".to_string();
+    let mut steps = 0;
+    while current_node_id != "ZZZ" {
+        println!("{}", current_node_id);
+        current_node_id = node_map
+            .get(&current_node_id)
+            .unwrap()
+            .next(instructions.next().unwrap());
+        steps += 1;
+    }
+    println!("{}", steps)
+}
+
+#[derive(Clone)]
+struct Node {
+    id: String,
+    left: String,
+    right: String,
+}
+
+impl Node {
+    fn next(&self, instruction: char) -> String {
+        match instruction {
+            'L' => self.left.clone(),
+            'R' => self.right.clone(),
+            _ => panic!("We've done a bad thing"),
+        }
+    }
+}
+
+#[derive(Debug)]
+struct ParseNodeError;
+
+impl FromStr for Node {
+    type Err = ParseNodeError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut strings = s.split_whitespace();
+        let id = strings.next().ok_or(ParseNodeError)?.to_string();
+        strings.next();
+        let left = strings.next().ok_or(ParseNodeError)?[1..4].to_string();
+        let right = strings.next().ok_or(ParseNodeError)?[0..3].to_string();
+
+        Ok(Node { id, left, right })
+    }
+}
+
+impl fmt::Debug for Node {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.left, self.right)
+    }
+}
