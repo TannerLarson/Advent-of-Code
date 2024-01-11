99225fcec4c301fc8addf547f9585244e45d67cd
Tanner Larson <tannerl@qualtrics.com>
2024-01-11 16:00:15 -0700

    Parsed input

--- 2023/rust/day13-23/src/main.rs
+++ 2023/rust/day13-23/src/main.rs
@@ -1,4 +1,61 @@
+use core::fmt;
+
+use grid::Grid;
+use itertools::Itertools;
+
 fn main() {
     let input = include_str!("ex1.txt");
-    s
+    let grids = input.split("\n\n").map(parse_grid).collect_vec();
+    print_grid(grids.first().unwrap())
+}
+
+#[derive(Default)]
+enum Object {
+    #[default]
+    Ash,
+    Rock,
+}
+
+impl fmt::Debug for Object {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", char::from(self))
+    }
+}
+
+impl From<char> for Object {
+    fn from(value: char) -> Self {
+        match value {
+            '.' => Object::Ash,
+            '#' => Object::Rock,
+            _ => panic!("Bad char: {}", value),
+        }
+    }
+}
+
+impl From<&Object> for char {
+    fn from(value: &Object) -> Self {
+        match value {
+            Object::Ash => '.',
+            Object::Rock => '#',
+        }
+    }
+}
+
+fn parse_grid(input: &str) -> Grid<Object> {
+    let num_cols = input.lines().next().unwrap().len();
+    let rows = input
+        .lines()
+        .flat_map(|line| line.chars().map(Object::from))
+        .collect_vec();
+
+    Grid::from_vec(rows, num_cols)
+}
+
+fn print_grid(grid: &Grid<Object>) {
+    for row in grid.iter_rows() {
+        for object in row {
+            print!("{:?} ", object)
+        }
+        println!()
+    }
 }
