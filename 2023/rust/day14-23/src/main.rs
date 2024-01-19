65c15c65a499ec93e03f219e80dcb392d130e14b
Tanner Larson <tannerl@qualtrics.com>
2024-01-18 18:14:28 -0700

    Continued work on part 2

--- 2023/rust/day14-23/src/main.rs
+++ 2023/rust/day14-23/src/main.rs
@@ -1,3 +1,5 @@
+use core::panic;
+
 use grid::Grid;
 use itertools::Itertools;
 
@@ -14,7 +16,7 @@ fn main() {
     let tilted = &platform.tilted(Direction::North);
     println!("Part 1: {}", tilted.get_load(Direction::North));
 
-    platform.spin(1000000000);
+    platform.spin(1);
     println!("Part 2: {}", platform.get_load(Direction::North))
 }
 
@@ -46,6 +48,7 @@ impl From<Rock> for char {
     }
 }
 
+#[derive(Clone, Copy, Debug)]
 enum Direction {
     North,
     West,
@@ -87,38 +90,64 @@ impl Platform {
         Self(tilted)
     }
 
+    fn directional_range(
+        &self,
+        dir: Direction,
+        i_line: usize,
+    ) -> Box<dyn Iterator<Item = (usize, usize)>> {
+        match dir {
+            Direction::North => Box::new((0..self.0.rows()).rev().map(move |i| (i, i_line))),
+            Direction::East => Box::new((0..self.0.cols()).map(move |i| (i_line, i))),
+            Direction::South => Box::new((0..self.0.rows()).map(move |i| (i, i_line))),
+            Direction::West => Box::new((0..self.0.cols()).rev().map(move |i| (i_line, i))),
+        }
+    }
+
     fn spin(&mut self, num_cycles: usize) {
-        for _ in 0..(num_cycles * 4) {
-            self.0.rotate_right();
-            for i_row in 0..self.0.rows() {
+        println!("Start");
+        print_grid(&self.0);
+        for i_rotation in 0..(num_cycles * 4) {
+            let (dir, it_i_line) = match i_rotation % 4 {
+                0 => (Direction::North, 0..self.0.cols()),
+                1 => (Direction::East, 0..self.0.rows()),
+                2 => (Direction::South, 0..self.0.cols()),
+                3 => (Direction::West, 0..self.0.rows()),
+                _ => panic!("At the disco: {}", i_rotation % 4),
+            };
+            println!("Direction: {:?}", dir);
+
+            for i_line in it_i_line {
                 // The idea here is to have two indexes, i_counter and i_updater
                 // i_counter will iterate until it finds a Cube rock, counting the number of round rocks it finds
                 // i_updater will wait for i_counter to find a Cube rock. Once it does, it will update the values
                 //   up to i_counter according to the number of round rocks found
                 // Once i_updater has caught up to i_counter, reset num_round.
                 let mut num_round = 0;
-                let mut i_counter = 0;
-                let mut i_updater = 0;
-                while i_counter <= self.0.cols() {
-                    let rock_at_counter = self.0.get(i_row, i_counter);
+                let mut counter_updater_diff = 0;
+
+                for counter_coord in self.directional_range(dir, i_line) {
+                    let mut it_updater_coord = self.directional_range(dir, i_line);
+                    let rock_at_counter = self.0.get(counter_coord.0, counter_coord.1);
                     if rock_at_counter.is_none() || *rock_at_counter.unwrap() == Rock::Cube {
                         // Use i_updater to update grid
-                        while i_updater < i_counter {
-                            if self.0.get(i_row, i_updater).unwrap() == &Rock::Cube {
-                            } else if i_updater >= i_counter - num_round {
-                                *self.0.get_mut(i_row, i_updater).unwrap() = Rock::Round
+                        for _ in 0..counter_updater_diff {
+                            let i_updater = it_updater_coord.next().unwrap();
+                            if counter_coord.0.abs_diff(i_updater.0) <= num_round
+                                && counter_coord.1.abs_diff(i_updater.1) <= num_round
+                            {
+                                *self.0.get_mut(i_updater.0, i_updater.1).unwrap() = Rock::Round
                             } else {
-                                *self.0.get_mut(i_row, i_updater).unwrap() = Rock::Empty
+                                *self.0.get_mut(i_updater.0, i_updater.1).unwrap() = Rock::Empty
                             }
-                            i_updater += 1;
                         }
-                        num_round = 0;
+                        counter_updater_diff = 0;
                     } else if *rock_at_counter.unwrap() == Rock::Round {
                         num_round += 1;
                     }
-                    i_counter += 1;
+                    counter_updater_diff += 1
                 }
             }
+            print_grid(&self.0);
         }
     }
 
