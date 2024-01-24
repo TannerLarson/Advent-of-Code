47072eff923063b2c95cf74eb1159a1634534d06
Tanner Larson <tannerl@qualtrics.com>
2024-01-24 11:55:59 -0700

    Added a from_str function for grid

--- lib/lib_rust/src/grid.rs
+++ lib/lib_rust/src/grid.rs
@@ -1,12 +1,19 @@
 use core::fmt;
-
 use grid::Grid;
+
+// This module extends the functionality of the grid crate with some functions I often use when working with grids
 
 pub trait GridExt<T> {
     fn print(&self);
+    fn from_str(string: &str) -> Grid<T>;
 }
 
-impl<T: fmt::Debug> GridExt<T> for Grid<T> {
+// The `where` clause forces any T to implement the defined traits
+impl<T> GridExt<T> for Grid<T>
+where
+    T: fmt::Debug,
+    T: From<char>,
+{
     fn print(&self) {
         for row in self.iter_rows() {
             for item in row {
@@ -15,5 +22,15 @@ impl<T: fmt::Debug> GridExt<T> for Grid<T> {
             println!()
         }
         println!()
+    }
+
+    fn from_str(string: &str) -> Grid<T> {
+        Grid::from_vec(
+            string
+                .lines()
+                .flat_map(|line| line.chars().map(T::from))
+                .collect(),
+            string.lines().next().unwrap().len(),
+        )
     }
 }
